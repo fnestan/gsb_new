87fdf3844082b5151af9f95f34c17f1a7166e753
frantz.nestan <ediscream@gmail.com>
2026-03-14 17:02:55 +0100

    make it better

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -10,13 +10,12 @@ use App\Entity\Visiteur;
 use App\Service\JwtTokenGenerator;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use Monolog\DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('rapport')]
+#[Route('rapports')]
 class ReportController extends AbstractController
 {
     private JwtTokenGenerator $tokenGenerator;
